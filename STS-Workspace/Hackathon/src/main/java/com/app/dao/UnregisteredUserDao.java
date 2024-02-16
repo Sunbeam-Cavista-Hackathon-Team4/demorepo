f78c0e6ceea2772074b80f6c9b3b70a17040de45
Kshitij-V-80352 <kvanarase1@gmail.com>
2024-02-16 14:15:56 +0530

    admin service and unregisteredDao

--- STS-Workspace/Hackathon/src/main/java/com/app/dao/UnregisteredUserDao.java
+++ STS-Workspace/Hackathon/src/main/java/com/app/dao/UnregisteredUserDao.java
@@ -1,9 +1,13 @@
 package com.app.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.entities.UnregisteredUser;
 
 public interface UnregisteredUserDao extends JpaRepository<UnregisteredUser, Long>{
+
+	List<UnregisteredUser> findByIsRegisteredIsFalse();
 
 }
--- STS-Workspace/Hackathon/src/main/java/com/app/service/AdminServiceImpl.java
+++ STS-Workspace/Hackathon/src/main/java/com/app/service/AdminServiceImpl.java
@@ -1,16 +1,27 @@
 package com.app.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.app.dao.UnregisteredUserDao;
 import com.app.dto.PatientDTO;
 import com.app.entities.Patient;
+import com.app.entities.UnregisteredUser;
 
 public class AdminServiceImpl implements AdminService{
 
+	@Autowired
+	private UnregisteredUserDao uudao;
+	@Autowired
+	private ModelMapper mapper;
+	
 	@Override
 	public List<PatientDTO> getUnregisteredPatients() {
-		// TODO Auto-generated method stub
-		return null;
+		List<UnregisteredUser> userList = uudao.findByIsRegisteredIsFalse();
+		return userList.stream().map(user-> mapper.map(user,PatientDTO.class)).collect(Collectors.toList());
 	}
 
 	@Override
