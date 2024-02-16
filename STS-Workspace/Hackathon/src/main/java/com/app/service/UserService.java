0d64cc29aa9fec714591406c7c1e0fce5f673a6f
GoravG <127304341+GoravG@users.noreply.github.com>
2024-02-16 14:49:49 +0530

    Added UnregisteredUserController

--- STS-Workspace/Hackathon/src/main/java/com/app/service/UserService.java
+++ STS-Workspace/Hackathon/src/main/java/com/app/service/UserService.java
@@ -1,8 +1,11 @@
 package com.app.service;
 
 import com.app.dto.Signup;
+import com.app.entities.UserEntity;
 
 public interface UserService {
 //sign up
 	Signup userRegistration(Signup reqDTO);
+	
+	UserEntity findByEmailAddress(String email);
 }
