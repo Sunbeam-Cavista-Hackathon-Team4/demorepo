07db4801173aac5f68940a02a4d881b78c25806d
hrutikpiprade <pipradehrutik@gmail.com>
2024-02-16 11:09:45 +0530

    doctor added

---
+++ STS-Workspace/Hackathon/src/main/java/com/app/entities/Doctor.java
@@ -0,0 +1,67 @@
+package com.app.entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="doctor")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor extends BaseEntity  {
+
+	@Column(length = 30,nullable = false)
+	private String name;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+	private LocalDate dob;
+	
+	@Enumerated(EnumType.ORDINAL)
+	private Gender gender;
+	
+	@Column(length = 30)
+	private String specialization;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+	private LocalDate joinDate;
+	
+	@Column(length = 15)
+	private String mobileNo;
+	
+	@Column(length = 30)
+	private String emailId;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id")
+	private Address address;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@OneToMany(mappedBy = "doctor",cascade = CascadeType.ALL)
+	private List<Appointment> appointments = new ArrayList<Appointment>();
+	
+	
+	
+	
+	
+}
